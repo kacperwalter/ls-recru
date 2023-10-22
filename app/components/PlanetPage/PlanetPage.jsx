c2ee30ce92f7e8ac468d270b0d2f7a63c1c5d5a2
kacperwalter <kacwal@poczta.fm>
2023-10-22 18:48:55 +0200

    Add displaying residents on planet page

--- app/components/PlanetPage/PlanetPage.jsx
+++ app/components/PlanetPage/PlanetPage.jsx
@@ -2,18 +2,31 @@ import Image from 'next/image'
 
 import Wrapper from '../UI/Wrapper/Wrapper'
 
+import splitId from '@/app/utils/splitId'
+import getCharacter from '@/app/utils/getCharacter'
+
 import './PlanetPage.scss'
 
 import temporaryPlanet from "@/public/temp-planet.webp"
 
-const PlanetPage = ({ data: { name, population, residents }}) => {
+const PlanetPage = async ({ data: { name, population, residents }}) => {
+  const residentsData = await Promise.all(residents.map(resident => getCharacter(splitId(resident))))
+
   return (
     <main className="planet-page">
       <Wrapper>
         <Image src={temporaryPlanet} alt={name} />
         <h1>Planet name: {name}</h1>
         <p>Population: {population}</p>
-        <p>Known residents: {residents}</p>
+
+        {residents.length > 0 && (
+         <div>
+          <p>Known residents: </p>
+          <ul>
+            {residentsData.map((resident, index) => <li key={index}>{resident.name}</li>)}
+          </ul>
+         </div>
+        )}
       </Wrapper>
     </main>
   )
