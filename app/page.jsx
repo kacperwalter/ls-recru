303e27ee7f09ffd81a749264573b70ffd7709e77
kacperwalter <kacwal@poczta.fm>
2023-10-24 18:52:18 +0200

    Move files temporary to another place

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
-import Sidebar from "./components/Sidebar/Sidebar"
-import CharactersPage from "./components/listings/CharactersPage/CharactersPage"
+// import Sidebar from "./components/Sidebar/Sidebar"
+// import CharactersPage from "./components/listings/CharactersPage/CharactersPage"
 
 const Home = () => {
   return (
     <>
-      <Sidebar />
-      <CharactersPage />
+      {/* <Sidebar />
+      <CharactersPage /> */}
     </>
   )
 }
