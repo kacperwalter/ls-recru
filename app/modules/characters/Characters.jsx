fa061eead224c435f3d3ad05ce7f75ab94b8c729
kacperwalter <kacwal@poczta.fm>
2023-10-25 17:56:36 +0200

    Add Card component

--- app/modules/characters/Characters.jsx
+++ app/modules/characters/Characters.jsx
@@ -1,16 +1,22 @@
 import getAllCharacters from "@/app/services/getAllCharacters"
 import Listing from "@/app/common/components/organisms/Listing/Listing"
+import Card from "@/app/common/components/organisms/Card/Card"
+
+import temporaryAvatar from "@/public/temp-avatar.jpeg"
 
 const Characters = async () => {
   const characters = await getAllCharacters(10)
 
   return (
     <Listing heading="Star Wars Characters">
-      <ul>
-        {characters.map(character => (
-          <li key={character.name}>{character.name}</li>
-        ))}
-      </ul>
+      {characters.map((character, index) => (
+        <Card
+          image={temporaryAvatar}
+          key={index}
+          id={index}
+          data={character}
+        />
+      ))}
     </Listing>
   )
 }
